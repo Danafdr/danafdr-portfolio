6e24a7d3b4db0102e06038d208602c213ac4d937
Danadirsha <danadirshafdr@gmail.com>
2026-06-07 14:55:16 +0700

    Revert Next.js proxy, rely on proper CORS domains

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://danafdr-portfolio-api.onrender.com/api/:path*',
-      },
-      {
-        source: '/sanctum/csrf-cookie',
-        destination: 'https://danafdr-portfolio-api.onrender.com/sanctum/csrf-cookie',
-      },
-      {
-        source: '/storage/:path*',
-        destination: 'https://danafdr-portfolio-api.onrender.com/storage/:path*',
-      }
-    ];
-  }
+  // Config here
 };
 
 export default nextConfig;
